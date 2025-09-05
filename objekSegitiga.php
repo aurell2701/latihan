e0b2e659a042e052b632bc659953cce8e52f8fee
aurell2701 <h1101241043@student.untan.ac.id>
2025-09-05 10:49:32 +0700

    Create objekSegitiga.php

---
+++ objekSegitiga.php
@@ -0,0 +1,11 @@
+<?php
+require_once "classSegitiga.php";
+
+// Objek segitiga 1
+$segitiga1 = new Segitiga(10, 8, 6, 8, 6);
+$segitiga1->tampilkanInfo();
+
+// Objek segitiga 2
+$segitiga2 = new Segitiga(5, 4.33, 5, 5, 5);
+$segitiga2->tampilkanInfo();
+?>
